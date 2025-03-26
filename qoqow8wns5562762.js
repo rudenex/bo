e715074b45de9fd03c83b202c3fa1061dd02b087
rudenex <bpl6192736@gmail.com>
2025-03-26 04:30:35 -0700

    Create qoqow8wns5562762.js

---
+++ qoqow8wns5562762.js
@@ -0,0 +1,3 @@
+document.getElementById('colorChangeButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#add8e6'; // Change background color to light blue
+});
